26de833f9714ae56e5f178a83c8c5e943e5ced31
Manuk <manuk_g@purlin.com>
2023-10-19 13:04:52 +0400

    Initial commit for Lessons exercies

---
+++ Lessons/LoopExercises/Program.cs
@@ -0,0 +1,43 @@
+﻿{
+
+
+    int sum = 0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        sum += i;
+    }
+
+    Console.WriteLine("The sum of numbers from 1 to 100 is: " + sum);
+
+}
+
+{
+    {
+        int sum = 0;
+        int i = 1;
+
+        while (i <= 100)
+        {
+            sum += i;
+            i++;
+        }
+
+        Console.WriteLine("The sum of numbers from 1 to 100 is: " + sum);
+    }
+}
+
+{
+    {
+        int sum = 0;
+        int i = 1;
+
+        do
+        {
+            sum += i;
+            i++;
+        } while (i <= 100);
+
+        Console.WriteLine("The sum of numbers from 1 to 100 is: " + sum);
+    }
+}
