29233eabd9235814cd4bf52bbf265b6d8c6845f1
Manuk <manuk_g@purlin.com>
2023-11-02 12:17:55 +0400

    last homework

--- Lessons/FunctionExercises/Program.cs
+++ Lessons/FunctionExercises/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿{
+    static void Main()
+    {
+        List<int> oddNumbers = new();
+        List<int> evenNumbers = new List<int>();
+
+        Console.WriteLine("Enter numbers (type 'exit' to finish):");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "exit")
+                break;
+
+            if (int.TryParse(input, out int number))
+            {
+                if (number % 2 == 0)
+                    evenNumbers.Add(number);
+                else
+                    oddNumbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number or type 'exit' to finish.");
+            }
+        }
+
+        Console.WriteLine("Odd numbers: ");
+        foreach (var odd in oddNumbers)
+        {
+            Console.Write(odd + " ");
+        }
+
+        Console.WriteLine("\nEven numbers: ");
+        foreach (var even in evenNumbers)
+        {
+            Console.Write(even + " ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿{
+    {
+    string planet;
+
+    Console.WriteLine("Enter a planet name:");
+    planet = Console.ReadLine();
+
+
+         switch (planet)
+        {
+            case "Mercury":
+                Console.WriteLine("The distance from the sun is 57,900,000km, the color is slate gray");
+                break;
+            case "Venus":
+                Console.WriteLine("The distance from the sun is 108,200,000km, the color is pearly white");
+                break;
+            case "Earth":
+                Console.WriteLine("The distance from the sun is 149,600,000km, the color is vibrant blue");
+                break;
+            case "Mars":
+                Console.WriteLine("The distance from the sun is 227,900,000km, the color is dusky red");
+                break;
+            case "Jupiter":
+                Console.WriteLine("The distance from the sun is 778,600,000km, the color is beige with brilliant red-brown belts");
+                break;
+            case "Saturn":
+                Console.WriteLine("The distance from the sun is 778,600,000km, the color is beige with brilliant red-brown belts");
+                break;
+            case "Uranus":
+                Console.WriteLine("The distance from the sun is 2,872,500,000km, the color is opaque blue");
+                break;
+            case "Neptune":
+                Console.WriteLine("The distance from the sun is 4,495,100,000km, the color is opaque blue");
+                break;
+            default:
+                break;
+        }
+        Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+    }
+}
+
+
+
+{
+    
+    
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine("The sum of numbers from 1 to 100 is: " + sum);
+
+}
+
+{
+    {
+        int sum = 0;
+        int i = 1;
+
+        while (i <= 100)
+        {
+            sum += i;
+            i++;
+        }
+
+        Console.WriteLine("The sum of numbers from 1 to 100 is: " + sum);
+    }
+}
+
+{    {
+        int sum = 0;
+        int i = 1;
+
+        do
+        {
+            sum += i;
+            i++;
+        } while (i <= 100);
+
+        Console.WriteLine("The sum of numbers from 1 to 100 is: " + sum);
+    }
+}
+
+{ 
+    {
+        int sum = SumNumbersFrom1To100();
+        Console.WriteLine("The sum of numbers from 1 to 100 is: " + sum);
+    }
+
+    static int SumNumbersFrom1To100()
+    {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
